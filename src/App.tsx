7dcfe3725b1451df63d401160c77eceeaf807f8d
Gakiri001 <gakirijulius@gmail.com>
2024-08-31 15:28:00 +0300

    created the todos and mapped them

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-  console.log(todo);
+  const [todos, setTodos] = useState<Todo[]>([])
+  console.log("Todo",todo);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if(todo){
+      setTodos([...todos,{id:Date.now(), todo:todo,isDone:false}])
+      setTodo("")
+    }
+  }
+  console.log("Todos",todos);
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,11 +4,12 @@ import "./Styles.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={(e) => handleAdd(e)}>
       <input
         type="text"
         placeholder="Enter a Task"

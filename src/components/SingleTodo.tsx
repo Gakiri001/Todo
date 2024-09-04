c4e605a92bcf6c3783c2f37067ef44343d24494e
Gakiri001 <gakirijulius@gmail.com>
2024-09-04 12:17:21 +0300

    working on the state management of the project

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,30 +1,83 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { MdDelete } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
 import { MdOutlineDone } from "react-icons/md";
 import { Todo } from "../model";
-import "./Styles.css"
+import "./Styles.css";
+import TodoList from "./TodoList";
 
-type Props ={
-  todo: Todo,
-  todos: Todo[],
+type Props = {
+  todo: Todo;
+  todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-}
+};
 
-const SingleTodo = ({todo,todos,setTodos}: Props) => {
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
+      ),
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id:number) => {
+    e.preventDefault();
+    setTodos(todos.map((todo) => (
+      todo.id === id?{...todo, todo:editTodo}:todo
+    )))
+    setEdit(false)
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit])
+
+
   return (
-    <form className="todo_single">
-    <span className="todos_single--text">
-      {todo.todo}
-    </span>
-    <div>
-      <span className="icon"><MdEdit/></span>
-      <span className="icon"><MdDelete/></span>
-      <span className="icon"><MdOutlineDone/></span>
-    </div>
-  </form>
-  )
+    <form className="todo_single" onSubmit={(e) => handleEdit(e,todo.id)}>
+      {edit ? (
+        <input
+        ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos_single--text"
+        />
+      ) : todo.isDone ? (
+        <s className="todos_single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos_single--text">{todo.todo}</span>
+      )}
 
+      <div>
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          <MdEdit />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <MdDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdOutlineDone />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTodo;
--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,29 @@ export interface Todo {
   todo: string;
   isDone: boolean;
 }
+
+// import React, {useReducer} from 'react'
+
+// type Actions = 
+//   | {type: "Add", payload:string}
+//   | {type: "remove", payload:number}
+//   | {type: "done", payload:number}
+
+// const ReducerExample = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, [])
+
+//   const TodoReducer = (state: Todo[],action:Actions) => {
+//     switch(action.type){
+//       case "Add":
+//         return [
+//           ...state,
+//           {id: Date.now(), todo: action.payload, isDone: false}
+//         ]
+//     }
+//   }
+//   return (
+//     <div>ReducerExample</div>
+//   )
+// }
+
+// export default ReducerExample
